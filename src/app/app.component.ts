ef5c874f7fab1c0f7f0a10550925d3a8b04c611a
RamMurari99 <rammurari3528@gmail.com>
2025-05-22 19:30:34 +0530

    updating the weather info UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,42 @@ import { WeatherService } from './weather.service';
   standalone: false,
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-   city: string = '';
-  weatherData: any;
-  error: string = '';
+// export class AppComponent {
+//    city: string = '';
+//   weatherData: any;
+//   error: string = '';
 
-  constructor(private weatherService: WeatherService) {}
+//   constructor(private weatherService: WeatherService) {}
+
+//   getWeather(): void {
+//     this.weatherService.getWeather(this.city).subscribe({
+//       next: (data) => {
+//         this.weatherData = data;
+//         this.error = '';
+//       },
+//       error: (err) => {
+//         this.weatherData = null;
+//         this.error = 'City not found or API error';
+//       },
+//     });
+//   }
+// }
+
+export class AppComponent {
+  city: string = '';
+  weatherData: any;               // For searched city
+  pidugurallaWeather: any;        // For default right-side weather
+  error: string = '';
+  currentDate: Date = new Date();
+
+  constructor(private weatherService: WeatherService) {
+    this.getPidugurallaWeather();
+
+    // Update time every second
+    setInterval(() => {
+      this.currentDate = new Date();
+    }, 1000);
+  }
 
   getWeather(): void {
     this.weatherService.getWeather(this.city).subscribe({
@@ -20,9 +50,20 @@ export class AppComponent {
         this.weatherData = data;
         this.error = '';
       },
-      error: (err) => {
+      error: () => {
         this.weatherData = null;
         this.error = 'City not found or API error';
+      },
+    });
+  }
+
+  getPidugurallaWeather(): void {
+    this.weatherService.getWeather('Piduguralla').subscribe({
+      next: (data) => {
+        this.pidugurallaWeather = data;
+      },
+      error: () => {
+        this.pidugurallaWeather = null;
       },
     });
   }
